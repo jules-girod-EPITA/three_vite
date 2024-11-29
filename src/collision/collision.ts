4d1dc78b7827f76917ee350a83358d50b25a9e4f
jules.girod <90521456+jules-girod-EPITA@users.noreply.github.com>
2024-11-29 19:41:27 +0100

    [deadcode] remove unused function

--- src/collision/collision.ts
+++ src/collision/collision.ts
@@ -1,16 +1,9 @@
 import {
     Box3,
-    BoxHelper, BufferGeometry,
-    EdgesGeometry, Line,
-    LineBasicMaterial,
-    LineSegments,
-    Mesh,
     Object3D,
-    Triangle,
     Vector3
 } from "three";
-import { gsap } from "gsap";
-import { cube, player } from "../terrain/initBoard";
+import { player } from "../terrain/initBoard";
 
 
 export function checkCollisionsTree(trees: Object3D[]) {
@@ -33,53 +26,6 @@ export function checkCollisionsTree(trees: Object3D[]) {
         if (distance < 10 && box.intersectsBox(new Box3().setFromObject(player)) && tree.userData.hasCollided + 4000 < new Date().getTime()) {
             player.setDeath();
             tree.userData.hasCollided = new Date().getTime();
-        }
-    });
-}
-
-export function checkCollisionsRocks(rocks: Object3D[]) {
-    if(player.isDead())
-        return;
-
-
-    rocks.forEach((rock) => {
-        const rockPosition = rock.getWorldPosition(new Vector3());
-        const playerPosition = player.getWorldPosition(new Vector3());
-        const distance = rockPosition.distanceTo(playerPosition);
-
-
-
-        if (!rock.userData.lastCollision) {
-            rock.userData.lastCollision = new Date().getTime() - 3000;
-        }
-
-
-        if (distance < 1 && rock.userData.lastCollision + 3000 < new Date().getTime()) {
-            rock.userData.lastCollision = new Date().getTime();
-
-            let front = rockPosition.z === playerPosition.z ? 0 : rockPosition.z > playerPosition.z ? 1 : -1;
-
-            let rotationX = front === 1 ? Math.PI / 2 : -Math.PI / 2;
-            gsap.to(cube.rotation, {
-                duration: 0.25,
-                x: rotationX,
-                onComplete: () => {
-                    gsap.to(cube.rotation, {
-                        duration: 2,
-                        x: 0,
-                    });
-                }
-            });
-            gsap.to(player.position, {
-                duration: 0.25,
-                y: 0.01,
-                onComplete: () => {
-                    gsap.to(player.position, {
-                        duration: 2,
-                        y: 0,
-                    });
-                }
-            })
         }
     });
 }
